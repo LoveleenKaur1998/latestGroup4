6d98692293d552891f974b69096cdf81e87a4f0d
anushadas110 <47036968+anushadas110@users.noreply.github.com>
2019-04-16 19:35:31 -0700

    modified helloworld app by anusha

--- Initial Commit/src/Helloworld.java
+++ Initial Commit/src/Helloworld.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 //added peeyush to helloworld
 //Added loveleen to helloworld
 //Added Peeyush to helloworld APP
+//Added anusha to helloworld APP
 public class Helloworld {
 
 	 private static void createAndShowGUI() {
@@ -11,7 +12,7 @@ public class Helloworld {
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-	        JLabel label1 = new JLabel("Hello World by Peeyush");
+	        JLabel label1 = new JLabel("Hello World by Anusha");
 	        frame.getContentPane().add(label1);
 
 	  	        frame.pack();
