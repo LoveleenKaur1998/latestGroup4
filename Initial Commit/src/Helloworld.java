20c92337acfbdee75871e039a6ff658eac595c77
Loveleen kaur <loveleengrewal1998@gmail.com>
2019-04-16 19:25:09 -0700

    Modified to a gui swing

--- Initial Commit/src/Helloworld.java
+++ Initial Commit/src/Helloworld.java
@@ -1,10 +1,28 @@
+import javax.swing.*;
+
 //added peeyush to helloworld
 //Added loveleen to helloworld
 public class Helloworld {
 
-	public static void main(String[] args) {
-	System.out.println("Hello World");
+	 private static void createAndShowGUI() {
 
-	}
+	        JFrame frame = new JFrame("HelloWorldSwing");
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+	        JLabel label = new JLabel("Hello World");
+	        frame.getContentPane().add(label);
+
+	  	        frame.pack();
+	        frame.setVisible(true);
+	    }
+
+	    public static void main(String[] args) {
+
+	        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	            public void run() {
+	                createAndShowGUI();
+	            }
+	        });
+	    }
 }
