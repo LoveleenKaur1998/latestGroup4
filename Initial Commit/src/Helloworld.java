103e3c82a8aa7e7cda8bcaa3fa91759aa15d67ba
SharmaPeeyush1998 <47036987+SharmaPeeyush1998@users.noreply.github.com>
2019-04-16 19:33:04 -0700

    UPdated helloworld app by adding the name

--- Initial Commit/src/Helloworld.java
+++ Initial Commit/src/Helloworld.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 //added peeyush to helloworld
 //Added loveleen to helloworld
+//Added Peeyush to helloworld APP
 public class Helloworld {
 
 	 private static void createAndShowGUI() {
@@ -10,8 +11,8 @@ public class Helloworld {
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-	        JLabel label = new JLabel("Hello World");
-	        frame.getContentPane().add(label);
+	        JLabel label1 = new JLabel("Hello World by Peeyush");
+	        frame.getContentPane().add(label1);
 
 	  	        frame.pack();
 	        frame.setVisible(true);
